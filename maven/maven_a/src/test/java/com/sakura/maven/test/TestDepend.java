1d1b6160d38f6aaf2e42a98b7fcb88543c59ce8f
Sakura <1980233736@qq.com>
2024-06-06 09:08:22 +0800

    测试依赖传递

---
+++ maven/maven_a/src/test/java/com/sakura/maven/test/TestDepend.java
@@ -0,0 +1,20 @@
+package com.sakura.maven.test;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+
+/**
+ * @Author Sakura
+ * @Date 2024/6/6 9:06
+ * @Version 1.0
+ */
+public class TestDepend {
+
+    @Test
+    public void testFastJson(){
+        //使用b工程的fastjson,体现依赖传递
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("key","value");
+        System.out.println(jsonObject.toJSONString());
+    }
+}
