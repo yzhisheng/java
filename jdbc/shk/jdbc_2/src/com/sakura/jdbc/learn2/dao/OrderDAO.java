32a4b51333ad9a5ae582f696aa0ebfbd7cc8c0dd
源稚生 <yzhisheng94666@gmail.com>
2024-06-09 17:09:54 +0800

    封装通用JDBC操作类BaseDao,定义接口，实现JDBC操作

---
+++ jdbc/shk/jdbc_2/src/com/sakura/jdbc/learn2/dao/OrderDAO.java
@@ -0,0 +1,12 @@
+package com.sakura.jdbc.learn2.dao;
+
+/**
+ * @Description
+ * @ClassName OrderDAO
+ * @Author Sakura
+ * @DateTime 2024-06-09 16:59:44
+ * @Version 1.0
+ */
+public interface OrderDAO {
+
+}
---
+++ jdbc/shk/jdbc_2/src/com/sakura/jdbc/learn2/dao/OrderDAOImpl.java
@@ -0,0 +1,11 @@
+package com.sakura.jdbc.learn2.dao;
+
+/**
+ * @Description
+ * @ClassName OrderDAOImpl
+ * @Author Sakura
+ * @DateTime 2024-06-09 16:59:52
+ * @Version 1.0
+ */
+public class OrderDAOImpl extends BaseDao implements OrderDAO {
+}
