b6ad8f19ae6e4f8f0aef9646cf82cc513a277504
源稚生 <yzhisheng94666@gmail.com>
2024-06-10 22:29:11 +0800

    lambda基础语法

---
+++ java/java8/src/main/java/com/sakura/java8/lambda/demo2/MyFun.java
@@ -0,0 +1,18 @@
+package com.sakura.java8.lambda.demo2;
+
+/**
+ * @Description
+ * @ClassName MyFun
+ * @Author Sakura
+ * @DateTime 2024-06-10 22:23:14
+ * @Version 1.0
+ */
+
+// * 函数式接口：接口中只有一个抽象方法的接口，称为函数式接口。 可以使用注解 @FunctionalInterface 修饰
+// * 			 可以检查是否是函数式接口
+@FunctionalInterface
+public interface MyFun {
+
+    public Integer getValue(Integer num);
+
+}
